f3574dd5dccf1f466ecaaab989701e8065afdc53
Yegon Japheth Kiptoo <jyegon504@gmail.com>
2023-05-22 13:38:57 +0300

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  def initialize(a, b, c)
+    @sides = [a, b, c]
+  end
+
+  def kind
+    validate_triangle
+
+    case @sides.uniq.size
+    when 1
+      :equilateral
+    when 2
+      :isosceles
+    when 3
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    raise TriangleError, "Enter a valid triangle" if @sides.include?(0)
+    raise TriangleError, "Enter a valid triangle" if @sides.any? { |side| side < 0 }
+    raise TriangleError, "Enter a valid triangle" unless triangle_inequality?
+  end
+
+  def triangle_inequality?
+    a, b, c = @sides
+    a + b > c && b + c > a && a + c > b
+  end
+
+  class TriangleError < StandardError; end
 end
